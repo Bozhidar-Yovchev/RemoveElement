21f700316f891ca8e1fd64614bbcc725fc2b347e
Bozhidar-Yovchev <bozhidar601@gmail.com>
2024-09-10 16:48:09 +0300

    Remove element

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Remove_Element
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter array:");
+            int[] nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Console.WriteLine("Enter comparing  value:");
+            int val = int.Parse(Console.ReadLine());
+            int[] arr =  new int[nums.Length];
+            int k = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i]!=val)
+                {
+                    arr[k] = nums[i];
+                    k++;
+                } 
+            }
+
+            Console.WriteLine(k);
+            
+            
+        }
+    }
+}
